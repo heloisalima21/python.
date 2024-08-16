b8fc311db2b20a045f8f3536f8a9dad5b3d0732b
heloisalima21 <heloisalimassa@gmail.com>
2024-08-16 18:14:13 +0000

    comandos iniciais.

---
+++ 1. tipos primitivos/tipagem dinamica.py
@@ -0,0 +1,12 @@
+import os
+
+os.system ("cls || clear")
+
+# Tipagem dinamica.
+nome = "Marta"
+idade = 22
+altura = 1.65
+
+print (f"Nome: {nome}")
+print(f"Idade: {idade}")
+print (f"Altura: {altura}")
---
+++ 1. tipos primitivos/tipagem estatica.py
@@ -0,0 +1,12 @@
+# Tipagem estática.
+nome : str #cadeia
+idade : int #inteiro
+altura : float #real
+
+nome  = "Marta"
+idade = 20
+altura = 1.65
+
+print(f"Nome: {nome}")
+print(f"idade: {idade}")
+print(f"Altura: {altura}")
